30e5a202d7595d896fdf96d3961c7e12c6aefab0
Zainoudine Soulé <tasmer.b@gmail.com>
2020-10-25 18:44:12 +0100

    add package mix-tailwindcss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,6 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
+require('mix-tailwindcss');
 
 mix.sass('./assets/src/theme-style.scss', './assets/css/style.css')
-    .options({
-        postCss: [
-            tailwindcss('./tailwind.config.js'),
-            require('autoprefixer')
-        ]
-    });
+    .tailwind('./tailwind.config.js');
