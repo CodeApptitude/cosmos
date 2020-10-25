6ef83ff83e56c42e1368246b36a8d2df2b8f9cda
Zainoudine Soulé <tasmer.b@gmail.com>
2020-10-25 18:46:14 +0100

    edit nav menu id

--- header.php
+++ header.php
@@ -11,21 +11,20 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="manifest" href="site.webmanifest">
     <link rel="apple-touch-icon" href="icon.png">
-    <?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <div id="page">
     <header>
-        <?php if ( has_nav_menu( 'primary' ) ) : ?>
-        <nav class="main-navigation" id="primary-navigation">
-            <?php wp_nav_menu( [
-                        'theme_location'  => 'primary',
-                        'menu_id'         => 'main-menu'
-                    ]); ?>
-        </nav>
-        <?php endif; ?>
+		<?php if ( has_nav_menu( 'primary-menu' ) ) : ?>
+            <nav class="main-navigation flex items-center justify-between flex-wrap bg-teal-500 p-6" id="primary-navigation">
+	            <?php wp_nav_menu( [
+		            'theme_location'  => 'primary-menu ',
+		            'menu_id'         => 'main-menu',
+		            'container_class' => 'w-full block flex-grow lg:flex lg:items-center lg:w-auto',
+	            ] ); ?>
+            </nav>
+		<?php endif; ?>
     </header>
     <div id="content">
